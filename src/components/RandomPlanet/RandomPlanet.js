04fcd15a3a660bffe4524ca107d01bb40a75e193
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-30 00:29:25 +0200

    randomPlannet api

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SwapiServices from '../../services/SwapiServices';
 
 import './RandomPlanet.css';
 
-const RandomPlanet = () => {
-    return (
-        <div className="planet jumbotron rounded">
-            <img className="planet__image"
-                src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
-            <div className="planet__descr">
-                <h4 className="descr__title">Planet Name</h4>
-                <ul className="descr__list list-group list-group-flush">
-                    <li className="descr__item list-group-item">
-                        <span className="descr__term">Population</span>
-                        <span className="descr__descr">123124</span>
-                    </li>
-                    <li className="descr__item list-group-item">
-                        <span className="descr__term">Rotation Period</span>
-                        <span className="descr__decr">43</span>
-                    </li>
-                    <li className="descr__item list-group-item">
-                        <span className="descr__term">Diameter</span>
-                        <span className="descr__descr">100</span>
-                    </li>
-                </ul>
+export default class RandomPlanet extends Component {
+    constructor() {
+        super();
+        this.state = {
+            id: null,
+            name: null,
+            population: null,
+            rotationPeriod: null,
+            diameter: null
+        }
+        this.swapiService = new SwapiServices();
+        this.updatePlannet()
+    }
+
+    updatePlannet() {
+        const id = Math.floor(Math.random() * 25) + 2;
+        this.swapiService.getPlanet(id).then((planet) => {
+            this.setState({
+                id,
+                name: planet.name,
+                population: planet.population,
+                rotationPeriod: planet.rotation_period,
+                diameter: planet.diameter
+            })
+        })
+    }
+
+    render() {
+        const { id, name, population, rotationPeriod, diameter } = this.state
+        return (
+            <div className="planet jumbotron rounded" >
+                <img className="planet__image"
+                    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+                <div className="planet__descr">
+                    <h4 className="descr__title">{name}</h4>
+                    <ul className="descr__list list-group list-group-flush">
+                        <li className="descr__item list-group-item">
+                            <span className="descr__term">Population</span>
+                            <span className="descr__descr">{population}</span>
+                        </li>
+                        <li className="descr__item list-group-item">
+                            <span className="descr__term">Rotation Period</span>
+                            <span className="descr__decr">{rotationPeriod}</span>
+                        </li>
+                        <li className="descr__item list-group-item">
+                            <span className="descr__term">Diameter</span>
+                            <span className="descr__descr">{diameter}</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default RandomPlanet;
