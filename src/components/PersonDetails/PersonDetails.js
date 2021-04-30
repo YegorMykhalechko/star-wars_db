985b795d88d9d4b5733b938254594b3053048617
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-30 23:39:54 +0200

    person loader fail

--- src/components/PersonDetails/PersonDetails.js
+++ src/components/PersonDetails/PersonDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import SwapiServices from '../../services/SwapiServices'
-// import Loader from '../Loader'
+import Loader from '../Loader'
 
 import './PersonDetails.css'
 
@@ -11,6 +11,7 @@ export default class PersonDetails extends Component {
 
     state = {
         person: null,
+        loading: true
     }
 
     componentDidMount() {
@@ -23,6 +24,13 @@ export default class PersonDetails extends Component {
         }
     }
 
+    onPlanetLoaded = (person) => {
+        this.setState({
+            person,
+            loading: false,
+        })
+    }
+
     updatePerson = () => {
         const { personId } = this.props
         if (!personId) {
@@ -30,67 +38,50 @@ export default class PersonDetails extends Component {
         }
         this.swapiService
             .getPerson(personId)
-            .then((person) => {
-                this.setState({ person })
-            })
+            .then(this.onPlanetLoaded)
     }
 
     render() {
+        const { person, loading } = this.state
         if (!this.state.person) {
             return <span>OOps</span>
         }
-        const {id, name, gender, birthYear, eyeColor} = this.state.person
+        const hasData = !(loading);
+        const loader = loading ? <Loader /> : null
+        const content = hasData ? <DetailView person={person} /> : null
         return (
             <div className="person__details card">
-                <img className="person__image"
-                    src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
-
-                <div className="person__card">
-                    <h4 className="person__title">{name}</h4>
-                    <ul className="person__list list-group list-group-flush">
-                        <li className="person__item list-group-item">
-                            <span className="person__term">Gender:</span>
-                            <span className="person__descr">{gender}</span>
-                        </li>
-                        <li className="person__item list-group-item">
-                            <span className="person__term">Birth Year:</span>
-                            <span className="person__descr">{birthYear}</span>
-                        </li>
-                        <li className="person__item list-group-item">
-                            <span className="person__term">Eye Color:</span>
-                            <span className="person__descr">{eyeColor}</span>
-                        </li>
-                    </ul>
-                </div>
+                {loader}
+                {content}
             </div>
         )
     }
 }
 
-// const DetailView = ({ person }) => {
-//     const { id, name, gender, birthYear, eyeColor } = person
-//     return (
-//         <React.Fragment>
-//             <img className="person__image"
-//                 src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+const DetailView = ({ person }) => {
+    const { id, name, gender, birthYear, eyeColor } = person
+    return (
+        <React.Fragment>
+            <img className="person__image"
+                src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
 
-//             <div className="person__card">
-//                 <h4 className="person__title">{name}</h4>
-//                 <ul className="person__list list-group list-group-flush">
-//                     <li className="person__item list-group-item">
-//                         <span className="person__term">Gender:</span>
-//                         <span className="person__descr">{gender}</span>
-//                     </li>
-//                     <li className="person__item list-group-item">
-//                         <span className="person__term">Birth Year:</span>
-//                         <span className="person__descr">{birthYear}</span>
-//                     </li>
-//                     <li className="person__item list-group-item">
-//                         <span className="person__term">Eye Color:</span>
-//                         <span className="person__descr">{eyeColor}</span>
-//                     </li>
-//                 </ul>
-//             </div>
-//         </React.Fragment>
-//     )
-// }
+            <div className="person__card">
+                <h4 className="person__title">{name}</h4>
+                <ul className="person__list list-group list-group-flush">
+                    <li className="person__item list-group-item">
+                        <span className="person__term">Gender:</span>
+                        <span className="person__descr">{gender}</span>
+                    </li>
+                    <li className="person__item list-group-item">
+                        <span className="person__term">Birth Year:</span>
+                        <span className="person__descr">{birthYear}</span>
+                    </li>
+                    <li className="person__item list-group-item">
+                        <span className="person__term">Eye Color:</span>
+                        <span className="person__descr">{eyeColor}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
+}
